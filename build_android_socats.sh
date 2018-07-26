7817eac29e987a3843fabefe41eb94e0aae5cd38
George Paitaris <wo@ipduh.com>
2018-07-26 03:50:16 +0300

    build socat for android

--- build_android_socats.sh
+++ build_android_socats.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 BUILD_ANDROID="./build_android.sh"
 SDKVS="9 12 13 14 15 16 17 18 19 21 22 23 24 26"
 ARCHS="arm arm64 mips mips64 x86 x86_64"
+
 for i in $SDKVS
   do
   for j in $ARCHS
@@ -9,4 +11,5 @@ for i in $SDKVS
     $BUILD_ANDROID $j $i 'rmbuild' 'verbose'
   done
 done
-find ./binaries -name filan -or -name socat -or -name procan |tee dose_android_socats_out
+
+find ./binaries -type f -exec shasum -a 256 {} \; |tee $0.out
